f03b716c4d7fd9c699bb9d46ce403faa7a4b2754
tomoaki-narita <output.org@gmail.com>
2022-06-21 15:37:40 +0900

    backup

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    //StoryBrain(構造体)をインスタンス化
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //upDateUIメソッドを呼ぶ
+        updateUI()
 
     }
-
-
+    
+    //2つのボタンをリンク(タップした方のボタンの情報がsenderに入る)
+    @IBAction func choiceMade(_ sender: Any) {
+        
+        //storyBrainのnextStoryメソッドを呼ぶ
+        storyBrain.nextStory(userChoice: (sender as AnyObject).currentTitle!)
+        //updateUIメソッドを呼ぶ
+        updateUI()
+    }
+    //updateUIメソッド
+    func updateUI() {
+        //storyBrainのgetStoryTitleメソッドで取得した値をstoruLabelのテキストに反映
+           storyLabel.text = storyBrain.getStoryTitle()
+        //ボタンのタイトルをそれぞれ、storyBrainのgetChoice1メソッドで取得した値を反映
+           choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+           choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+       }
+    
 }
 
